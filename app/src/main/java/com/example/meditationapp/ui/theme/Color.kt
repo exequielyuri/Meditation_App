0c200a6806ef6c03e9e9c21f8fbc6044035ad003
Exequiel Garcia <leiuqxre@gmail.com>
2022-10-17 13:58:40 +0800

    Remove unused colors

--- app/src/main/java/com/example/meditationapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/meditationapp/ui/theme/Color.kt
@@ -7,26 +7,10 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val PurpleLight = Color(0xFF9C2EDB)
-val PurpleLightLight = Color(0xEE815F8E)
-
-val DarkYellowGradient = Color(0xFFFF9A00)
-val LightYellowGradient = Color(0xFFFFD590)
-
-val Pink = Color(0xFFF5E6FC)
-
-val Yellow10 = Color(0xffFFF7EA)
-val Yellow20 = Color(0xffFAC87F)
-val Yellow30 = Color(0xffFCA453)
-
-val Brown10 = Color(0xffF5D3B3)
-val Brown20 = Color(0xffEDB987)
-val Brown30 = Color(0xffE0BB99)
-val Brown40 = Color(0xffC38A56)
-
 val Purple10 = Color(0xffB184C1)
 val Purple20 = Color(0xffF6EFFC)
 val Purple30 = Color(0xffDDBFF6)
 val Purple40 = Color(0xffB184C1)
+val Purple45 = Color(0xff8754AE)
 val Purple50 = Color(0xff572C78)
-val Purple60 = Color(0xff2C133C)
+val Purple60 = Color(0xff735888)
