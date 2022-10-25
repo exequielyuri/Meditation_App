26f26c653db6a0cedacbfd3e7f0577ea89e69fb0
Exequiel Garcia <leiuqxre@gmail.com>
2022-10-26 07:30:09 +0800

    Add default values for journal entry

--- app/src/main/java/com/example/meditategg/model/JournalEntry.kt
+++ app/src/main/java/com/example/meditategg/model/JournalEntry.kt
@@ -2,12 +2,12 @@ package com.example.meditategg.model
 
 data class JournalEntry(
     val id: String = "",
-    val day: Int,
-    val date: Int,
-    val month: Int,
-    val year: Int,
-    val meditationName: String,
-    val durationMin: Int,
-    val durationSec: Int,
+    val day: Int = -1,
+    val date: Int = -1,
+    val month: Int = -1,
+    val year: Int = -1,
+    val meditationName: String = "",
+    val durationMin: Int = -1,
+    val durationSec: Int = -1,
     val content: String = "",
 )
