d489af5dc4ce2d915ce2f92ccb736550d07f915a
Exequiel Garcia <leiuqxre@gmail.com>
2022-11-04 08:37:12 +0800

    Add helper functions

---
+++ app/src/main/java/com/example/meditategg/common/ext/ModifierExt.kt
@@ -0,0 +1,8 @@
+package com.example.meditategg.common.ext
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+
+fun Modifier.example(): Modifier {
+    return this.fillMaxWidth()
+}
